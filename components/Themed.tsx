e88a41696b7fffc9366102b489b347fec8241821
Eesha-Jain <eesha.jain801@gmail.com>
2021-08-12 20:33:20 -0700

    Finished answer tab for now

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -35,7 +35,7 @@ export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+  return <DefaultText style={[{ fontFamily: 'hn-light' }, { color }, style]} {...otherProps} />;
 }
 
 export function View(props: ViewProps) {
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -9,9 +9,14 @@ import storage from "@react-native-async-storage/async-storage";
 import { firebase } from '../util/firebaseInit.js';
 import sharedStyles from '../styles/SharedStyles.ts';
 import { blue1, blue2, blue3, blue4, green, red, gray, white } from '../util/colors.ts';
+import { LineChart, BarChart, PieChart, ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit';
 
 export default function TabTwoScreen() {
   const [polls, setPolls] = useState([]);
+  const list = [blue1, blue2, blue3, blue4, blue3, blue2, blue1];
 
   useEffect(() => {
     const makeRequest = async () => {
@@ -42,12 +47,59 @@ export default function TabTwoScreen() {
 
       <ScrollView style={{paddingTop: 20}}>
         {polls.map((item) => {
+          let arr = [];
+          var count = 0;
+          for (var i = 0; i < item.choices.length; i++) { count += item.responses[i]; }
+          for (var i = 0; i < item.choices.length; i++) { arr[i] = ((item.responses[i] * 100) / (count)).toFixed(0); }
+
+          let pollData = [];
+          for (var i = 0; i < item.choices.length; i++) {
+            let dic = {
+              name: item.choices[i],
+              population: item.responses[i],
+              color: list[i],
+              legendFontColor: '#7F7F7F',
+              legendFontSize: 15
+            };
+            pollData.push(dic);
+          }
+
           return (
             <View key={item.id} style={sharedStyles.poll}>
               <View style={{flexDirection: 'row', backgroundColor: 'transparent', marginBottom: 5, justifyContent: 'space-between'}}>
                 <View style={{height: 15, width: 15, backgroundColor: green, borderRadius: 50}}></View>
                 <Text style={{fontFamily: 'hn-bold', fontSize: 18, marginLeft: 10, marginRight: 10}}>{item.title}</Text>
+                <View></View>
               </View>
+
+              <ScrollView>
+                <View style={{flexDirection: 'row', backgroundColor: 'transparent', width: '100%', flex: 1, flexWrap: 'wrap'}}>
+                  <View style={{marginRight: 10, backgroundColor: 'transparent', width: '25%'}}>
+                  <PieChart data={pollData} width={win.width * 0.25} height={win.width * 0.25} hasLegend={false}
+                    chartConfig={{
+                      backgroundColor: '#e26a00',
+                      backgroundGradientFrom: '#fb8c00',
+                      backgroundGradientTo: '#ffa726',
+                      decimalPlaces: 2, // optional, defaults to 2dp
+                      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                      style: {
+                        borderRadius: 16
+                      }
+                    }} accessor="population" backgroundColor="transparent" paddingLeft="15"
+                  />
+                  </View>
+                  <View style={{backgroundColor: 'transparent'}}>
+                    {arr.map((percent, i) => {
+                      return (
+                        <View key={i} style={{backgroundColor: 'transparent', flexDirection: 'row', width: '60%'}}>
+                          <Text style={{fontSize: 17, color: list[i]}}>{item.choices[i]} ({percent}%)</Text>
+                          <View style={{marginLeft: 10, borderRadius: 50, width: percent + '%', backgroundColor: list[i]}}></View>
+                        </View>
+                      );
+                    })}
+                  </View>
+                </View>
+              </ScrollView>
             </View>
           );
         })}
